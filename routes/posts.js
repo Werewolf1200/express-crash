d5116ab94112afdc31d9498388aeff2abdd4d8bb
Saúl Camacho <electrohead1200@gmail.com>
2024-05-22 11:43:26 -0600

    Routes & req.params

---
+++ routes/posts.js
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+const router = Router();
+
+let posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'},
+]
+
+// Get all posts
+router.get('/', (req, res) => {
+
+    const limit = parseInt(req.query.limit);
+
+    if (!isNaN(limit) && limit > 0) {
+        res.status(200).json(posts.slice(0, limit));
+    } else {
+        res.status(200).json(posts);
+    }
+}) 
+
+// Get One Post
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+       res.status(400).json({msg: `A post with the id of ${id} was not found`}); 
+    } else {
+        res.status(200).json(post);
+    }
+}) 
+
+export default router
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+
+import posts from './routes/posts.js';
 
 // Especificar archivo env --env-file=.env
 const port = process.env.PORT || 8000;
@@ -10,13 +12,7 @@ const app = express();
 // app.use(express.static(path.join(__dirname, 'public')));
 /* .join une el nombre un directorio y un archivo */
 
-let posts = [
-    {id: 1, title: 'Post One'},
-    {id: 2, title: 'Post Two'},
-    {id: 3, title: 'Post Three'},
-]
-app.get('/api/posts', (req, res) => {
-    res.json(posts);
-}) 
+// Routes (Middleware)
+app.use('/api/posts', posts)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
