0c7fbd96298b63594de7bce1858ddcad1b313cc9
Saúl Camacho <electrohead1200@gmail.com>
2024-05-25 13:29:47 -0600

    Making requests from the Frontend and creating a Form

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import path from 'path';
-
+import { fileURLToPath } from 'url';
 import posts from './routes/posts.js';
 
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
 import notFound from './middleware/notFound.js';
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Especificar archivo env --env-file=.env
 const port = process.env.PORT || 8000;
@@ -20,7 +24,7 @@ app.use(express.urlencoded({ extended: false })); // Allow us to send data from 
 app.use(logger);
 
 // Set Up a Static folder
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 /* .join une el nombre un directorio y un archivo */
 
 // Routes (Middleware)
